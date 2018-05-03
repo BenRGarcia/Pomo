ec7d0f3930aca8e4ceaa77fb59c1268a2dd71e4a
Ben Garcia <ben@seebenprogram.com>
2018-05-03 10:28:32 -0400

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var router = express.Router()
 
+/**
+ * HTML Routes
+ */
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
