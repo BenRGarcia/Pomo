17660aee29c983a7d1ccf44da25f8f0649973bed
Your Name <melissarburnham@gmail.com>
2018-05-05 16:19:00 -0400

    add task function attempt

--- public/javascripts/class_view.js
+++ public/javascripts/class_view.js
@@ -1,41 +1,44 @@
 $(function () {
-  // **select students to assign tasks
-  $('#pick_students').on('click', function (event) {
-    var id = $(this).data('id')
-    var studentsPicked = $(this).data('studentsPicked')
+/**
+ * Handle when teacher is viewing class
+ */
 
-    $.ajax('/api/students/' + id, {
-      type: 'PUT'
-    }).then(
-      function () {
-        console.log('login successful')
-        window.location.reload()
-      }
-    )
+ // checkbox[data-add=student]
+ //grab selected students and assign task, time duration, and coin value
+  $('body').on('click', 'submit', e => {
+    // get the ids of the students 
+    var studentArr = []
+    // var students = checkbox[data-add=student]
+    var student_uuid = $(e.target).data('student_uuid') 
+
+    if(student_uuid.checked = true){
+      studentArr.push(student_uuid)    
+    } 
+
+    $('#js-modal-task-assignment').modal('show') // has form input and submit button
+
+    var task_name = $('#task_name').val().trim()
+    var timer_duration = $('#timer_duration').val().trim()
+    var coin_value = $('#coin_value').val().trim()
+
+    var taskAssignment = {task_name, timer_duration, coin_value}
+    
+      
+    $('#js-modal-task-assignment').on('submit', 'form', e => {
+      e.preventDefault()
+      studentArr.forEach(element => {
+        $.post('/api/task/', taskAssignment)
+        .then(
+          function () {
+            console.log('assigned task to', studentArr[element])
+            window.location.reload()
+          }
+        )
+        .catch(err => console.error(err.message))
+      });
+    })
   })
-  // assign tasks, coin values, and timers to students selected
-  $('#assign_submit').on('click', function (event) {
-    var id = $(this).data('id')
-    var tasks = $('#tasksAssigned').val().trim()
-    var coinValue = $('#coinValue').val().trim()
-    var timerDuration = $('#timerDuration').val().trim()
-
-    var newValues = {
-      name: tasks,
-      coin_value: coinValue,
-      timer_duration: timerDuration
-    }
-
-    $.ajax('/api/task/' + id, {
-      type: 'PUT',
-      data: newValues
-    }).then(
-      function () {
-        console.log('added newValues to' + id)
-        window.location.reload()
-      }
-    )
-  })
+ 
   //* *add coins to each student
   $('.add_coins').on('click', function (event) {
     var id = $(this).data('id')
@@ -58,17 +61,6 @@ $(function () {
     }).then(
       function () {
         console.log('deleted coins from' + id)
-        window.location.reload()
-      }
-    )
-  })
-  // view the store
-  $('#storefront').on('click', function (event) {
-    $.ajax('/api/store', {
-      type: 'GET'
-    }).then(
-      function () {
-        console.log('view store successful')
         window.location.reload()
       }
     )
@@ -106,15 +98,4 @@ $(function () {
       }
     )
   })
-  // go back to teacher dashboard
-  $('.dashboard').on('click', function (event) {
-    $.ajax('/api/teachers/dashboard', {
-      type: 'GET'
-    }).then(
-      function () {
-        console.log('login successful')
-        window.location.reload()
-      }
-    )
-  })
-})
+
