1c8f65b55263f77865dad2f42fbaeb7ffaaf3200
Ben Garcia <ben@seebenprogram.com>
2018-05-06 13:14:55 -0400

    build .hbs routers

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,52 @@ var router = express.Router()
  * HTML Routes
  */
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+// Home page
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' })
+})
+
+// Class store (hard coded feature)
+router.get('/store', (req, res, next) => {
+  res.render('store')
+})
+
+// Teacher log in
+router.get('/teacher/login', (req, res, next) => {
+  res.render('teacherLogin')
+})
+
+// Teacher sign up
+router.get('/teacher/signup', (req, res, next) => {
+  res.render('teacherSignup')
+})
+
+// Teacher dashboard
+router.get('/teacher/:uuid/dashboard', (req, res, next) => {
+  // Run sequelize query here, pass to .hbs
+  res.render('teacherDashboard', {/* give me db results */})
+})
+
+// Teacher create, edit class
+router.get('/teacher/:uuid/edit', (req, res, next) => {
+  // Run sequelize query here, pass to .hbs
+  res.render('classEdit', {/* give me db results */ })
+})
+
+// Teacher manage class (day to day stuff)
+router.get('/teacher/:uuid/manage', (req, res, next) => {
+  // Run sequelize query here, pass to .hbs
+  res.render('classManage', {/* give me db results */ })
+})
+
+// Student login
+router.get('/student/login', (req, res, next) => {
+  res.render('studentLogin')
+})
+
+// Student dashboard
+router.get('/student/:uuid/dashboard', (req, res, next) => {
+  res.render('studentDashboard', {/* give me db results */})
 })
 
 module.exports = router
