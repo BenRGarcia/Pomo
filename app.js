e9b7258650cb3c66c9cb6d4075afbb5087252a80
Ben Garcia <ben@seebenprogram.com>
2018-05-07 14:34:57 -0400

    Add auth

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+require('dotenv').config()
 var createError = require('http-errors')
 var express = require('express')
+var helmet = require('helmet')
+var session = require('express-session')
+var RedisStore = require('connect-redis')(session)
+var redis = require('redis')
+var client = redis.createClient(process.env.REDIS_URL || null)
+var passport = require('./config/passport.js')
 var path = require('path')
 var logger = require('morgan')
 var sassMiddleware = require('node-sass-middleware')
 var hbs = require('hbs')
 var favicon = require('serve-favicon')
-require('dotenv').config()
-
 var indexRouter = require('./routes/index')
 var teacherRouter = require('./routes/dashboard.js')
 var studentRouter = require('./routes/student.js')
-
+// Additional .hbs setup required before using csurf
+// var csurf = require('csurf')
+// var csrfProtection = csrf({ cookie: true })
 var app = express()
+var limiter = require('express-limiter')(app, client)
+app.use(helmet())
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // view engine setup
@@ -28,19 +37,53 @@ app.use(sassMiddleware({
   indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: false
 }))
+
+app.use(session({
+  cookie: {
+    /* **** Enable for production **** */
+    // domain: 'herokuapp.com',
+    // path: '/',
+    httpOnly: true,
+    sameSite: true,
+    secure: false, // change to 'true' for production
+    maxAge: 30 * 60 * 1000 // 30 minutes
+  },
+  name: 'sessionId',
+  secret: 'change this secret to a new random string before deployment',
+  resave: false,
+  saveUninitialized: false,
+  rolling: true,
+  store: new RedisStore({ client })
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: false
+}))
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+limiter({
+  path: '*',
+  method: 'all',
+  lookup: ['connection.remoteAddress'],
+  total: 200, // 200 requests per hour
+  expire: 1000 * 60 * 60,
+  skipHeaders: true
+})
 
 app.use('/', indexRouter)
 app.use('/api/teacher', teacherRouter)
 app.use('/api/students', studentRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
+app.use((req, res, next) => next(createError(404)))
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
