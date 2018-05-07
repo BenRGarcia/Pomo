41ce9237304a53f459e808d7b1d43a208f3fd442
Ben Garcia <ben@seebenprogram.com>
2018-05-07 08:46:17 -0400

    Delete unused dep

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
-var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var sassMiddleware = require('node-sass-middleware')
 var hbs = require('hbs')
