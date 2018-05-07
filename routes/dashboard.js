02fa0a4f31f0a5349cf9ce29bfe7b05836c1ced6
Ben Garcia <ben@seebenprogram.com>
2018-05-07 11:54:42 -0400

    import authentication middleward

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var db = require('../models')
+var isAuthenticated = require('./utils/isAuthenticated.js')
 
 /**
  * Discover API path '/api/teacher'
