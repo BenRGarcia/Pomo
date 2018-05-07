29c37badbe0467d3c4cf0be8979d1580bdd07709
Ben Garcia <ben@seebenprogram.com>
2018-05-07 11:35:22 -0400

    reorg, build path

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,45 @@
 var express = require('express')
 var router = express.Router()
 var db = require('../models')
+var isAuthenticated = require('./utils/isAuthenticated.js')
+
+/***************
+ * HTML Routes *
+ ***************/
 
 /**
- * HTML Routes
+ * Static pages
  */
 
 // Home page
-router.get('/', (req, res, next) => {
-  res.render('index')
-})
+router.get('/', (req, res, next) => res.render('index'))
 
-// Class store (hard coded feature)
-router.get('/store', (req, res, next) => {
-  res.render('store')
-})
+// Student login page
+router.get('/student/login', (req, res, next) => res.render('studentLogin'))
 
-// Teacher log in
-router.get('/teacher/login', (req, res, next) => {
-  res.render('teacherLogin')
-})
+// Teacher log in page
+router.get('/teacher/login', (req, res, next) => res.render('teacherLogin'))
 
-// Teacher sign up
-router.get('/teacher/signup', (req, res, next) => {
-  res.render('teacherSignup')
-})
+// Teacher sign up page
+router.get('/teacher/signup', (req, res, next) => res.render('teacherSignup'))
 
-// Teacher dashboard
-/* router.get('/teacher/:uuid/dashboard', (req, res, next) => {
+// Class store page (hard coded future-feature)
+router.get('/teacher/store', (req, res, next) => res.render('store'))
+
+/**
+ * Dynamic pages
+ */
+
+// Teacher dashboard page
+router.get('/teacher/:uuid/dashboard', /* isAuthenticated, */(req, res, next) => {
   var uuid = req.params.uuid
-  // Run sequelize query here, pass to .hbs
-  // db.User.find({...}, where: {...}, include: {...})
-    .then(resp => {
-      res.render('teacherDashboard', resp)
-    })
-}) */
+  // Davis - how do we query the DB for all the teacher's classes?
+  db.User.find({ where: { uuid }, include: { model: db.Class } })
+    // Sending json response simply to debug - will delete before production
+    .then(resp => res.json(resp))
+    // .then(resp => res.render('teacherDashboard', resp))
+    .catch(err => res.json(err))
+})
 
 // Teacher create, edit class
 /* router.get('/teacher/:uuid/edit', (req, res, next) => {
@@ -55,11 +60,6 @@ router.get('/teacher/signup', (req, res, next) => {
       res.render('classManage', resp)
     })
 }) */
-
-// Student login
-router.get('/student/login', (req, res, next) => {
-  res.render('studentLogin')
-})
 
 // Student dashboard
 /* router.get('/student/:uuid/dashboard', (req, res, next) => {
