b383c15ba686d136cd0cf791592c66329c8a275a
Ben Garcia <ben@seebenprogram.com>
2018-05-07 18:28:06 -0400

    increase limit

--- config/limiter.js
+++ config/limiter.js
@@ -2,7 +2,7 @@ module.exports = {
   path: '*',
   method: 'all',
   lookup: ['connection.remoteAddress'],
-  total: 200, // 200 requests per hour
+  total: 2000, // 2000 requests per hour
   expire: 1000 * 60 * 60,
   skipHeaders: true
 }
