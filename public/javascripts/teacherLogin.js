afc64ac45ca259e56ba986c3fff87dfa1105f973
Ben Garcia <ben@seebenprogram.com>
2018-05-07 19:29:05 -0400

    refactor, functionality achieved

--- public/javascripts/teacherLogin.js
+++ public/javascripts/teacherLogin.js
@@ -3,29 +3,40 @@ $(function () {
   /**
    * When user submits login/signup form
    */
-  $('body').on('submit', '#login', e => {
-  // Prevent default reloading of page
+  $('body').on('submit', '#js-form-signup-or-login', e => {
+    console.log(`teacher login/signup was just submitted`)
+    // Prevent default reloading of page
     e.preventDefault()
 
     // Get values from user input
-    var radioButtonValue = $('input[name=isLogin]:checked').val()
-    var email = $('#email').val().trim()
-    var password = $('#password').val().trim()
-    var _csrf = $('#_csrf').val().trim()
-    var user = { email, password, _csrf }
+    var email = $('#js-email').val().trim()
+    var password = $('#js-password').val()
+    // var _csrf = $('#_csrf').val().trim()
+    var user = { email, password }
     var path
 
     // Clear elements on the screen
-    $('#signUpEmail').val('')
-    $('#password').val('')
-    $('#_csrf').val('')
+    $('#js-email').val('')
+    $('#js-password').val('')
 
+    // Determine whether user is on login or signup page
+    if (window.location.pathname === '/teacher/signup') {
+      console.log(`if condition executed`)
+      path = '/api/teacher/signup'
+    } else if (window.location.pathname === '/teacher/login') {
+      console.log(`else condition executed`)
+      path = '/api/teacher/login'
+    }
+    console.log(`\n================\n`)
+    console.log(path)
+    console.log(`================\n`)
     // Send post request
-    $.post('/api/teacher/login', user)
+    $.post(path, user)
       .then(res => {
         if (res.isAuthenticated) {
-          window.location.replace(res.redirect)
+          window.location.replace(res.redirectPath)
         } else {
+          window.alert('incorrect username/password combination')
           // somehow alert user "Incorrect email/password combination"
           // $('#alert-div').addClass('text-red')
           // $('#alert-div').text('Incorrect email/password combination')
