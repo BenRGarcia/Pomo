fd50f6356ca44b344f84a82bd15ce85eca4e2a4e
Ben Garcia <ben@seebenprogram.com>
2018-05-08 09:17:39 -0400

    Update code style

--- public/javascripts/teacher_login.js
+++ public/javascripts/teacher_login.js
@@ -1,38 +1,28 @@
-
 $(function () {
   $('body').on('submit', '#js-form-signup-or-login', e => {
-    console.log(`teacher login/signup was just submitted`)
-    // Prevent default reloading of page
     e.preventDefault()
 
-    // Get values from user input
     var email = $('#js-email').val().trim()
     var password = $('#js-password').val()
     // var _csrf = $('#_csrf').val().trim()
     var user = { email, password }
-    var path
 
-    // Clear elements on the screen
     $('#js-email').val('')
     $('#js-password').val('')
 
-    // Determine whether user is on login or signup page
-    if (window.location.pathname === '/teacher/signup') {
-      console.log(`if condition executed`)
-      path = '/api/teacher/signup'
-    } else if (window.location.pathname === '/teacher/login') {
-      console.log(`else condition executed`)
-      path = '/api/teacher/login'
-    }
-    // Send post request
+    var path
+
+    if (window.location.pathname === '/teacher/signup') path = '/api/teacher/signup'
+    else if (window.location.pathname === '/teacher/login') path = '/api/teacher/login'
+
     $.post(path, user)
       .then(res => {
-        if (res.isAuthenticated) {
-          window.location.replace(res.redirectPath)
-        } else {
-          window.alert('incorrect username/password combination')
-        }
+        if (res.isAuthenticated) window.location.replace(res.redirectPath)
+        else window.alert('incorrect username/password combination')
       })
-      .catch(err => console.error(err.message))
+      .catch(err => {
+        console.error(err)
+        window.alert(err.statusText)
+      })
   })
 })
