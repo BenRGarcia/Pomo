b3b78a6106fc6123e6642da8d0cf8cb53792d71b
Ben Garcia <ben@seebenprogram.com>
2018-05-07 20:30:36 -0400

    update code style

--- public/javascripts/dashboard_student.js
+++ public/javascripts/dashboard_student.js
@@ -6,26 +6,26 @@ $(function () {
 
     var start_update = {student_name, current_time}
 
-    $.post('/api/task/start', start_update) 
+    $.post('/api/task/start', start_update)
       .then(
-      function () {
-        console.log('start time added')
-      }
-    )
+        function () {
+          console.log('start time added')
+        }
+      )
   })
 
   // when student hits done
   $('body').on('click', '#done', e => {
     var student_name = $(e.target).data('student_name')
-    var isDone = $(e.target).data("isDone")
+    var isDone = $(e.target).data('isDone')
 
-    var isDone_update= {student_name, isDone}
+    var isDone_update = {student_name, isDone}
 
-    $.put('/api/task/start', isDone_update) 
+    $.put('/api/task/start', isDone_update)
       .then(
-      function () {
-        console.log('isDone has been updated to true')
-      }
-    )
+        function () {
+          console.log('isDone has been updated to true')
+        }
+      )
   })
 })
