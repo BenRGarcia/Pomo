40be5dd4bf7638e8c42ad894e200a035acabc708
Ben Garcia <ben@seebenprogram.com>
2018-05-08 13:51:34 -0400

    Send db resp to hbs template

--- routes/index.js
+++ routes/index.js
@@ -50,8 +50,7 @@ router.get('/teacher/class/:uuid/manage', /* isAuthenticated, */(req, res, next)
       required: false
     }]
   })
-    // .then(students => res.render('classManage', { students, layout: '/layouts/layoutTeacher' }))
-    .then(resp => res.json(resp))
+    .then(students => res.render('classManage', { students, layout: '/layouts/layoutTeacher' }))
     .catch(err => res.json(err))
 })
 
