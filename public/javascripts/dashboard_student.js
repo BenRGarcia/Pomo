a94984004ef23c3a7f45c5542c37bbff29019ab7
Ben Garcia <ben@seebenprogram.com>
2018-05-09 13:18:18 -0400

    Build front end timer components

--- public/javascripts/dashboard_student.js
+++ public/javascripts/dashboard_student.js
@@ -1,31 +1,72 @@
 $(function () {
-  // start the timer
-  $('body').on('click', '#start', e => {
-    var student_name = $(e.target).data('student_name')
-    var current_time = date.now()
+  // Event listener for timer start
+  $('body').on('click', '#js-timer-start', e => startTask())
 
-    var start_update = {student_name, current_time}
+  // Event listener for timer done
+  $('body').on('click', '#js-timer-done', e => completeTask())
 
-    $.post('/api/task/start', start_update)
-      .then(
-        function () {
-          console.log('start time added')
-        }
-      )
-  })
+  // Retrieve data value to determine if timer has started
+  var timerIsStarted = !!$('#js-timer-raw-data').data('start-time')
 
-  // when student hits done
-  $('body').on('click', '#done', e => {
-    var student_name = $(e.target).data('student_name')
-    var isDone = $(e.target).data('isDone')
+  // Conditional execution if timer is started
+  if (timerIsStarted) {
+    // Get timer data from DOM html elements
+    var startTime = $('#js-timer-raw-data').data('start-time')
+    var duration = $('#js-timer-raw-data').data('duration')
+    // Create set interval
+    var intervalId = setInterval(() => {
+      // Calculate time remaining
+      var secondsRemaining = calculateSecondsRemaining(startTime, duration)
+      // Format time remaining
+      var timerObj = formatTimer(secondsRemaining)
+      // Update DOM with time remaining
+      updateDOM(timerObj)
+      // If time is up...
+      if (secondsRemaining <= 0) {
+        // ...clear timer, send `isDone` post to server
+        clearInterval(intervalId)
+        completeTask()
+      }
+    }, 1000)
+  }
 
-    var isDone_update = {student_name, isDone}
+  // Returns number of seconds remaining
+  function calculateSecondsRemaining (unixStartTime, duration) {
+    var secondsPassed = (Date.now() - unixStartTime) / 1000
+    return duration - secondsPassed
+  }
 
-    $.put('/api/task/start', isDone_update)
-      .then(
-        function () {
-          console.log('isDone has been updated to true')
-        }
-      )
-  })
+  // Returns object with minutes and seconds props
+  function formatTimer (secondsRemaining) {
+    var minutes = Math.floor(secondsRemaining / 60)
+    var seconds = secondsRemaining % 60
+    return { minutes, seconds }
+  }
+
+  // Updates DOM with current timer data
+  function updateDOM ({ minutes, seconds }) {
+    $('#js-timer-minutes').val(minutes)
+    $('#js-timer-seconds').val(seconds)
+  }
+
+  // Returns the task's UUID
+  function getTaskUUID () {
+    return $('#js-task-raw-data').data('uuid')
+  }
+
+  // Sends POST request that timer is started
+  function startTask () {
+    var taskUUID = getTaskUUID
+    $.post('/api/task/timer/start', { taskUUID })
+      .then(() => window.location.reload())
+      .catch(() => window.location.reload())
+  }
+
+  // Send post request that task is complete
+  function completeTask () {
+    var taskUUID = getTaskUUID
+    $.post('/api/task/timer/done', { taskUUID })
+      .then(() => window.location.reload())
+      .catch(() => window.location.reload())
+  }
 })
