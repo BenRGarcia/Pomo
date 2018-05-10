dd2cf4d224763a4bc4b6a499d12b5fad06826f1b
Ben Garcia <ben@seebenprogram.com>
2018-05-10 13:22:01 -0400

    Delete unused routes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -63,49 +63,4 @@ router.route('/class/add')
       .catch(err => console.error(err))
   })
 
-// For when a teacher adds students to the class (student name, )
-router.route('/:uuid_teacher/class/:uuid_/edit')
-  // GET list of all students in a class
-  .get((req, res, next) => {
-    db.Class.findAll({ include: [db.User, db.Student] })
-      .then(dbClass => res.json(dbClass))
-      .catch(err => console.error(err))
-  })
-  // POST requests for this path
-  .post((req, res, next) => {
-    db.Class.create(req.body).then(function (dbClass) {
-      res.json(dbClass)
-    })
-  })
-  // PUT requests for this path
-  .put((req, res, next) => {
-    db.Class.update(
-      req.body, {
-        where: {
-          id: req.body.id
-        }
-      }).then(function (dbClass) {
-      res.json(dbClass)
-    })
-  })
-
-router.route('/api/classes/:id')
-  // GET requests for this path
-  .get((req, res, next) => {
-    db.Class.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.Student]
-    }).then(function (dbClass) {
-      res.json(dbClass)
-    })
-  })
-
-router.route('/api/student/dashboard')
-  // GET requests for this path
-  .get((req, res, next) => {
-    res.render('index', { title: 'Express' })
-  })
-
 module.exports = router
