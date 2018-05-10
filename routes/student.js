d952450077eccb9480cd0c0f122d67355b0961de
Ben Garcia <ben@seebenprogram.com>
2018-05-10 10:25:20 -0400

    Delete unused routes

--- routes/student.js
+++ routes/student.js
@@ -31,33 +31,4 @@ router.route('/login')
       .catch(() => res.json({ redirectPath: `/student/login` }))
   })
 
-router.route('/api/students')
-  // GET requests for this path
-  .get((req, res, next) => {
-    db.Student.findAll({ include: [db.Class, db.Task] })
-      .then((dbStudent) => res.json(dbStudent))
-  })
-  // POST requests for this path
-  .post((req, res, next) => {
-    db.Student.create(req.body)
-      .then((dbStudent) => res.json(dbStudent))
-  })
-  // PUT requests for this path
-  .put((req, res, next) => {
-    db.Student.update(req.body, { where: { id: req.body.id } })
-      .then((dbStudent) => res.json(dbStudent))
-  })
-  // DELETE requests for this path
-  .delete((req, res, next) => {
-    db.Student.destroy({ where: { id: req.params.id } })
-      .then((dbStudent) => res.json(dbStudent))
-  })
-
-router.route('/api/students/:id')
-  // GET requests for this path
-  .get((req, res, next) => {
-    db.Student.findOne({ where: { id: req.params.id }, include: [db.Class, db.Task] })
-      .then((dbStudent) => res.json(dbStudent))
-  })
-
 module.exports = router
