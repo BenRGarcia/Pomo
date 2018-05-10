d06e262fe9f70a66d3d46138cbc472be17228ae2
Ben Garcia <ben@seebenprogram.com>
2018-05-09 21:21:59 -0400

    Update time formatting

--- public/javascripts/dashboard_student.js
+++ public/javascripts/dashboard_student.js
@@ -1,5 +1,5 @@
 $(function () {
-  // Event listener for timer start
+// Event listener for timer start
   $('body').on('click', '#js-timer-start', e => startTask())
 
   // Event listener for timer done
@@ -10,12 +10,12 @@ $(function () {
 
   // Conditional execution if timer is started
   if (timerIsStarted) {
-    // Get timer data from DOM html elements
+  // Get timer data from DOM html elements
     var startTime = $('#js-timer-raw-data').data('start-time')
     var duration = $('#js-timer-raw-data').data('duration')
     // Create set interval
     var intervalId = setInterval(() => {
-      // Calculate time remaining
+    // Calculate time remaining
       var secondsRemaining = calculateSecondsRemaining(startTime, duration)
       // Format time remaining
       var timerObj = formatTimer(secondsRemaining)
@@ -23,7 +23,7 @@ $(function () {
       updateDOM(timerObj)
       // If time is up...
       if (secondsRemaining <= 0) {
-        // ...clear timer, send `isDone` post to server
+      // ...clear timer, send `isDone` post to server
         clearInterval(intervalId)
         completeTask()
       }
@@ -40,19 +40,19 @@ $(function () {
   function formatTimer (secondsRemaining) {
     var minutes = Math.floor(secondsRemaining / 60)
     var seconds = Math.floor(secondsRemaining % 60)
+    console.log(`minutes: ${minutes} seconds: ${seconds}`)
+    seconds = seconds <= 9 ? '0' + seconds.toString() : seconds
     return { minutes, seconds }
   }
 
   // Updates DOM with current timer data
   function updateDOM ({ minutes, seconds }) {
-    console.log(`minutes: ${minutes} seconds: ${seconds}`)
     $('#js-timer-minutes').text(minutes)
     $('#js-timer-seconds').text(seconds)
   }
 
   // Returns the task's UUID
   function getTaskUUID () {
-    console.log(`$('#js-timer-raw-data').data('uuid') is: ${$('#js-timer-raw-data').data('uuid')}`)
     return $('#js-timer-raw-data').data('uuid')
   }
 
@@ -66,7 +66,7 @@ $(function () {
 
   // Send post request that task is complete
   function completeTask () {
-    var taskUUID = getTaskUUID
+    var taskUUID = getTaskUUID()
     $.ajax({
       url: '/api/task/timer/done',
       method: 'PUT',
